fc16c2b61a37b613891305cef6adb8526654e227
MisteryKid <abade3548@gmail.com>
2025-05-03 14:56:07 +0900

    start

---
+++ src/main/java/com/example/follow_3/Follow3Application.java
@@ -0,0 +1,13 @@
+package com.example.follow_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Follow3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Follow3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/follow_3/Follow3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.follow_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Follow3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
